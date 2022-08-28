46b7c7a1e639fd217559ff6d4dc0b9b20043055e
Zhao KK <kikakkz@hotmail.com>
2022-08-28 06:18:42 +0000

    Use new api manager

--- cmd/review-service/run.go
+++ cmd/review-service/run.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"time"
-
 	"github.com/NpoolPlatform/review-service/api"
 	db "github.com/NpoolPlatform/review-service/pkg/db"
-	msgcli "github.com/NpoolPlatform/review-service/pkg/message/client"
-	msglistener "github.com/NpoolPlatform/review-service/pkg/message/listener"
-	msg "github.com/NpoolPlatform/review-service/pkg/message/message"
-	msgsrv "github.com/NpoolPlatform/review-service/pkg/message/server"
 
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
@@ -36,16 +30,6 @@ var runCmd = &cli.Command{
 			}
 		}()
 
-		if err := msgsrv.Init(); err != nil {
-			return err
-		}
-		if err := msgcli.Init(); err != nil {
-			return err
-		}
-
-		go msglistener.Listen()
-		go msgSender()
-
 		return grpc2.RunGRPCGateWay(rpcGatewayRegister)
 	},
 }
@@ -61,23 +45,7 @@ func rpcGatewayRegister(mux *runtime.ServeMux, endpoint string, opts []grpc.Dial
 		return err
 	}
 
-	apimgrcli.Register(mux)
+	apimgrcli.Register(mux) //nolint
 
 	return nil
-}
-
-func msgSender() {
-	id := 0
-	for {
-		err := msgsrv.PublishExample(&msg.Example{
-			ID:      id,
-			Example: "hello world",
-		})
-		if err != nil {
-			logger.Sugar().Errorf("fail to send example: %v", err)
-			return
-		}
-		id++
-		time.Sleep(3 * time.Second)
-	}
 }
